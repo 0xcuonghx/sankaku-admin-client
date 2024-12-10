b71582235b9860012be87f991c83f55cf22ac8d7
cuonghx <cuonghx@gmail.com>
2024-12-10 23:07:05 +0700

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,12 @@ import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
 import App from "./App";
-import Dashboard from "./dashboard/Dashboard";
-import SignIn from "./sign-in/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* <App /> */}
-      {/* <Dashboard /> */}
-      {/* <SignIn /> */}
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
