6929857e989ec5fa5e1e124236661917d348fd09
cuonghx <cuonghx@gmail.com>
2024-12-22 21:48:57 +0700

    Fix lint

--- src/dashboard/components/NavbarBreadcrumbs.tsx
+++ src/dashboard/components/NavbarBreadcrumbs.tsx
@@ -8,7 +8,7 @@ import { useLocation } from "react-router";
 const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   margin: theme.spacing(1, 0),
   [`& .${breadcrumbsClasses.separator}`]: {
-    color: (theme.vars || theme).palette.action.disabled,
+    color: theme.palette.action.disabled,
     margin: 1,
   },
   [`& .${breadcrumbsClasses.ol}`]: {
