7d55588d26b4894f0d0dbfdfc2579f82c2e56e97
cuonghx <cuonghx@gmail.com>
2024-12-22 15:26:43 +0700

    Contract Management

--- src/dashboard/contract-management/ContractManagement.tsx
+++ src/dashboard/contract-management/ContractManagement.tsx
@@ -1,8 +1,16 @@
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useRecurringExecutionPlans } from "./hooks/useRecurringExecutionPlans";
-import { Box, Button, Chip, Switch, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Chip,
+  Link,
+  Paper,
+  Switch,
+  Typography,
+} from "@mui/material";
 import { formatUnits } from "viem";
-import { AddOutlined } from "@mui/icons-material";
+import { AddOutlined, PauseCircleOutline } from "@mui/icons-material";
 
 export default function ContractManagement() {
   const { data, isLoading } = useRecurringExecutionPlans();
@@ -75,64 +83,103 @@ export default function ContractManagement() {
 
   return (
     <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: 10,
-        }}
-      >
-        <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-          Subscription Plans
-        </Typography>
-        <Button startIcon={<AddOutlined />} variant="contained">
-          Add
-        </Button>
-      </div>
-      <DataGrid
-        sx={{ width: "100%" }}
-        loading={isLoading}
-        autoHeight
-        disableRowSelectionOnClick
-        rows={data}
-        columns={columns}
-        getRowClassName={(params) =>
-          params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
-        }
-        initialState={{
-          pagination: { paginationModel: { pageSize: 20 } },
-        }}
-        pageSizeOptions={[10, 20, 50]}
-        disableColumnResize
-        getRowId={(row) => row.planId}
-        density="compact"
-        slotProps={{
-          filterPanel: {
-            filterFormProps: {
-              logicOperatorInputProps: {
-                variant: "outlined",
-                size: "small",
-              },
-              columnInputProps: {
-                variant: "outlined",
-                size: "small",
-                sx: { mt: "auto" },
-              },
-              operatorInputProps: {
-                variant: "outlined",
-                size: "small",
-                sx: { mt: "auto" },
-              },
-              valueInputProps: {
-                InputComponentProps: {
+      <Paper sx={{ padding: 2, marginBottom: 2 }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: 10,
+          }}
+        >
+          <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+            Information
+          </Typography>
+          <Button startIcon={<PauseCircleOutline />} variant="contained">
+            Pause
+          </Button>
+        </div>
+        <div>
+          <Typography component="p">
+            Contract Address:{" "}
+            <Link
+              href={
+                "https://amoy.polygonscan.com/address/0x9c8a9F28005b5b0ECA5cafb4B0d234e71a38293c"
+              }
+              target="_blank"
+            >
+              0x9c8a9F28005b5b0ECA5cafb4B0d234e71a38293c
+            </Link>
+          </Typography>
+          <Typography>
+            Status:{" "}
+            <Chip
+              label={true ? "Active" : "Inactive"}
+              color={true ? "success" : "error"}
+              size="small"
+            />
+          </Typography>
+        </div>
+      </Paper>
+      <Paper sx={{ padding: 2 }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: 10,
+          }}
+        >
+          <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+            Subscription Plans
+          </Typography>
+          <Button startIcon={<AddOutlined />} variant="contained">
+            Add
+          </Button>
+        </div>
+        <DataGrid
+          sx={{ width: "100%" }}
+          loading={isLoading}
+          autoHeight
+          disableRowSelectionOnClick
+          rows={data}
+          columns={columns}
+          getRowClassName={(params) =>
+            params.indexRelativeToCurrentPage % 2 === 0 ? "even" : "odd"
+          }
+          initialState={{
+            pagination: { paginationModel: { pageSize: 20 } },
+          }}
+          pageSizeOptions={[10, 20, 50]}
+          disableColumnResize
+          getRowId={(row) => row.planId}
+          density="compact"
+          slotProps={{
+            filterPanel: {
+              filterFormProps: {
+                logicOperatorInputProps: {
                   variant: "outlined",
                   size: "small",
                 },
+                columnInputProps: {
+                  variant: "outlined",
+                  size: "small",
+                  sx: { mt: "auto" },
+                },
+                operatorInputProps: {
+                  variant: "outlined",
+                  size: "small",
+                  sx: { mt: "auto" },
+                },
+                valueInputProps: {
+                  InputComponentProps: {
+                    variant: "outlined",
+                    size: "small",
+                  },
+                },
               },
             },
-          },
-        }}
-      />
+          }}
+        />
+      </Paper>
     </Box>
   );
 }
